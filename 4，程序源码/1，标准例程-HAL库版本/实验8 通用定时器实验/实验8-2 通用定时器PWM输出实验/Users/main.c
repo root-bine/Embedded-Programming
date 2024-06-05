620d3c9b6f39e1cedb708e9fff34ec308a0f47ac
源問 <67728844+root-bine@users.noreply.github.com>
2024-06-06 00:19:04 +0800

    Add files via upload

---
+++ 4，程序源码/1，标准例程-HAL库版本/实验8 通用定时器实验/实验8-2 通用定时器PWM输出实验/Users/main.c
@@ -0,0 +1,86 @@
+/**
+ ******************************************************************************
+ * @file     main.c
+ * @author   正点原子团队(ALIENTEK)
+ * @version  V1.0
+ * @date     2023-08-01
+ * @brief    通用定时器PWM输出实验
+ * @license  Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ******************************************************************************
+ * @attention
+ * 
+ * 实验平台:正点原子 M48Z-M3最小系统板STM32F103版
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ ******************************************************************************
+ */
+
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./SYSTEM/delay/delay.h"
+#include "./BSP/LED/led.h"
+#include "./BSP/TIM/gtim.h"
+
+
+/**
+ * @brief       显示实验信息
+ * @param       无
+ * @retval      无
+ */
+void stm32f103c8t6_show_mesg(void)
+{
+    /* 串口输出实验信息 */
+    printf("\n");
+    printf("********************************\r\n");
+    printf("STM32\r\n");
+    printf("GTIM PWM Test\r\n");
+    printf("ATOM@ALIENTEK\r\n");
+    printf("********************************\r\n");
+    printf("\r\n");
+}
+
+
+int main(void)
+{
+    uint32_t ledpwmval = 0;
+    uint8_t dir = 1;
+    
+    HAL_Init();                                                 /* 初始化HAL库 */
+    sys_stm32_clock_init(RCC_PLL_MUL9);                         /* 设置时钟, 72Mhz */
+    delay_init(72);                                             /* 延时初始化 */
+    usart_init(115200);                                         /* 初始化串口 */
+    gtim_timx_pwm_chy_init(500 - 1, 72 - 1);                    /* 1Mhz的计数频率,2Khz的PWM. */
+    stm32f103c8t6_show_mesg();                                  /* 实验提示信息 */
+
+    while (1)
+    {
+       delay_ms(10);
+        
+        /* 根据方向修改ledpwmval */
+        if (dir == 1)
+        {
+            ledpwmval++;
+        }
+        else
+        {
+            ledpwmval--;
+        }
+        
+        /* 当ledpwmval大于300时，方向改为递减 */
+        if (ledpwmval > 300)
+        {
+            dir = 0;
+        }
+        
+        /* 当ledpwmval等于0时，方向改为递增 */
+        if (ledpwmval == 0)
+        {
+            dir = 1;
+        }
+
+        /* 修改比较值控制占空比 */
+        __HAL_TIM_SET_COMPARE(&g_timx_pwm_chy_handle, GTIM_TIMX_PWM_CHY, ledpwmval);
+    }
+}
